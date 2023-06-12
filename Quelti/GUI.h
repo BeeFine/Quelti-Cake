9a5bdb40745efcdab4e23ca1351e482c091599e2
Daniil Kyznetsovツ <131686227+BeeFine@users.noreply.github.com>
2023-06-12 23:29:27 +0300

    0.2.1
    Encryption Only

---
+++ Quelti/GUI.h
@@ -0,0 +1,34 @@
+/* GUI class */
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <random>
+#include <stdint.h>
+#include <thread>
+
+#include "Languages/languages.h"
+#include "Colors/colors.h"
+
+class GUI
+{
+public:
+	 void initializationComponents();
+	 void Getline(std::string* strInput);
+	 void waitTime(uint32_t time);
+
+	 uint8_t mainScreen(bool animation);
+	 uint8_t encryptionScreen1();
+	 std::vector<std::string> encryptionScreen2();
+	 void EncryptionFinalScreen(std::vector<std::string> Answer);
+};
+
+class Encryption
+{
+public:
+	void EncryptionMethod(std::vector<std::string> vectorAnswers, std::string* numAlphabets);
+	void DecryptionMethod(std::vector<std::string> vectorAnswers);
+
+private:
+	const std::string GLOBAL_ALPHABET = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!@#$%^&*()_+-=[]{}|;':,./<>?`\"\\~ ";
+};
---
+++ Quelti/Languages/languages.h
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+
+class Languages
+{
+private:
+	std::string english(uint8_t textNumber);
+
+public:
+	std::string getText(uint8_t textNumber);
+};
